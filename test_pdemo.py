4fa1a7485963b6bb4fce136169b71a9d1c234ba7
Plutohui <zaszchen@gmail.com>
2021-06-22 10:19:27 +0800

    First commit add test_pdemo.py file and selenium webdriver (chrome)

---
+++ test_pdemo.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+def test_add():
+    a = 2
+    b = 3
+    assert a == b
+
+def sub(x):
+    return x - 1
+
+def test_answer():
+
+    assert sub(6) == 5
+
+
+if __name__ == '__main__':
+    pytest.main()
